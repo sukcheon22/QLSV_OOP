5a6fbd5c7e0b95bbffeea092713425adb41357cb
Quach Thai Duong <duong.qt210253@sis.hust.edu.vn>
2024-01-01 18:45:13 +0700

    SVdesignAccomplished

---
+++ QLSV_OOP/TTinDuNo.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace QLSV_OOP
+{
+    partial class TTinDuNo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Số tiền học phí chưa thanh toán";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(319, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // TTinDuNo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "TTinDuNo";
+            this.Size = new System.Drawing.Size(661, 106);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
--- QLSV_OOP/frmStudentAnalysis.Designer.cs
+++ QLSV_OOP/frmStudentAnalysis.Designer.cs
@@ -54,15 +54,14 @@
             // menuStripStudent
             // 
             this.menuStripStudent.BackColor = System.Drawing.Color.RoyalBlue;
-            this.menuStripStudent.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStripStudent.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStripStudent.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exportFileToolStripMenuItem,
             this.backToolStripMenuItem});
             this.menuStripStudent.Location = new System.Drawing.Point(0, 0);
             this.menuStripStudent.Name = "menuStripStudent";
-            this.menuStripStudent.Padding = new System.Windows.Forms.Padding(6, 1, 0, 1);
-            this.menuStripStudent.Size = new System.Drawing.Size(1311, 31);
+            this.menuStripStudent.Padding = new System.Windows.Forms.Padding(5, 1, 0, 1);
+            this.menuStripStudent.Size = new System.Drawing.Size(1165, 26);
             this.menuStripStudent.TabIndex = 1;
             this.menuStripStudent.Text = "menuStrip1";
             // 
@@ -71,7 +70,7 @@
             this.exportFileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exportFileToolStripMenuItem.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.exportFileToolStripMenuItem.Name = "exportFileToolStripMenuItem";
-            this.exportFileToolStripMenuItem.Size = new System.Drawing.Size(100, 29);
+            this.exportFileToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
             this.exportFileToolStripMenuItem.Text = "Xuất file";
             this.exportFileToolStripMenuItem.Click += new System.EventHandler(this.exportFileToolStripMenuItem_Click);
             // 
@@ -80,7 +79,7 @@
             this.backToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.backToolStripMenuItem.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.backToolStripMenuItem.Name = "backToolStripMenuItem";
-            this.backToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
             this.backToolStripMenuItem.Text = "Quay lại";
             this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
             // 
@@ -98,18 +97,18 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.txtNumCountK66);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(165, 122);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(147, 98);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(984, 181);
+            this.panel1.Size = new System.Drawing.Size(875, 145);
             this.panel1.TabIndex = 12;
             // 
             // txtNumCountStudent
             // 
-            this.txtNumCountStudent.Location = new System.Drawing.Point(469, 135);
-            this.txtNumCountStudent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumCountStudent.Location = new System.Drawing.Point(417, 108);
+            this.txtNumCountStudent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumCountStudent.Name = "txtNumCountStudent";
-            this.txtNumCountStudent.Size = new System.Drawing.Size(76, 26);
+            this.txtNumCountStudent.Size = new System.Drawing.Size(68, 22);
             this.txtNumCountStudent.TabIndex = 20;
             this.txtNumCountStudent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -117,10 +116,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(297, 139);
+            this.label8.Location = new System.Drawing.Point(264, 111);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(66, 29);
+            this.label8.Size = new System.Drawing.Size(54, 24);
             this.label8.TabIndex = 19;
             this.label8.Text = "Tổng:";
             // 
@@ -128,19 +127,19 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(554, 79);
+            this.label3.Location = new System.Drawing.Point(492, 63);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 29);
+            this.label3.Size = new System.Drawing.Size(82, 24);
             this.label3.TabIndex = 18;
             this.label3.Text = "Khóa 68:";
             // 
             // txtNumCountK67
             // 
-            this.txtNumCountK67.Location = new System.Drawing.Point(726, 20);
-            this.txtNumCountK67.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumCountK67.Location = new System.Drawing.Point(645, 16);
+            this.txtNumCountK67.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumCountK67.Name = "txtNumCountK67";
-            this.txtNumCountK67.Size = new System.Drawing.Size(76, 26);
+            this.txtNumCountK67.Size = new System.Drawing.Size(68, 22);
             this.txtNumCountK67.TabIndex = 17;
             this.txtNumCountK67.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -148,19 +147,19 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(554, 22);
+            this.label5.Location = new System.Drawing.Point(492, 18);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(98, 29);
+            this.label5.Size = new System.Drawing.Size(82, 24);
             this.label5.TabIndex = 14;
             this.label5.Text = "Khóa 67:";
             // 
             // txtNumCountK68
             // 
-            this.txtNumCountK68.Location = new System.Drawing.Point(726, 78);
-            this.txtNumCountK68.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumCountK68.Location = new System.Drawing.Point(645, 62);
+            this.txtNumCountK68.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumCountK68.Name = "txtNumCountK68";
-            this.txtNumCountK68.Size = new System.Drawing.Size(76, 26);
+            this.txtNumCountK68.Size = new System.Drawing.Size(68, 22);
             this.txtNumCountK68.TabIndex = 16;
             this.txtNumCountK68.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -168,29 +167,29 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(554, 75);
+            this.label7.Location = new System.Drawing.Point(492, 60);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(0, 29);
+            this.label7.Size = new System.Drawing.Size(0, 24);
             this.label7.TabIndex = 15;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(14, 79);
+            this.label6.Location = new System.Drawing.Point(12, 63);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 29);
+            this.label6.Size = new System.Drawing.Size(82, 24);
             this.label6.TabIndex = 13;
             this.label6.Text = "Khóa 66:";
             // 
             // txtNumCountK65
             // 
-            this.txtNumCountK65.Location = new System.Drawing.Point(186, 20);
-            this.txtNumCountK65.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumCountK65.Location = new System.Drawing.Point(165, 16);
+            this.txtNumCountK65.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumCountK65.Name = "txtNumCountK65";
-            this.txtNumCountK65.Size = new System.Drawing.Size(76, 26);
+            this.txtNumCountK65.Size = new System.Drawing.Size(68, 22);
             this.txtNumCountK65.TabIndex = 10;
             this.txtNumCountK65.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -198,19 +197,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 22);
+            this.label2.Location = new System.Drawing.Point(12, 18);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 29);
+            this.label2.Size = new System.Drawing.Size(82, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Khóa 65:";
             // 
             // txtNumCountK66
             // 
-            this.txtNumCountK66.Location = new System.Drawing.Point(186, 78);
-            this.txtNumCountK66.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumCountK66.Location = new System.Drawing.Point(165, 62);
+            this.txtNumCountK66.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumCountK66.Name = "txtNumCountK66";
-            this.txtNumCountK66.Size = new System.Drawing.Size(76, 26);
+            this.txtNumCountK66.Size = new System.Drawing.Size(68, 22);
             this.txtNumCountK66.TabIndex = 8;
             this.txtNumCountK66.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -218,40 +217,40 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(14, 75);
+            this.label4.Location = new System.Drawing.Point(12, 60);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.Size = new System.Drawing.Size(0, 24);
             this.label4.TabIndex = 7;
             // 
             // studentDataGridView
             // 
             this.studentDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.studentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.studentDataGridView.Location = new System.Drawing.Point(60, 326);
-            this.studentDataGridView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.studentDataGridView.Location = new System.Drawing.Point(53, 261);
+            this.studentDataGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.studentDataGridView.Name = "studentDataGridView";
             this.studentDataGridView.RowHeadersWidth = 62;
-            this.studentDataGridView.Size = new System.Drawing.Size(1182, 340);
+            this.studentDataGridView.Size = new System.Drawing.Size(1051, 272);
             this.studentDataGridView.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(459, 68);
+            this.label1.Location = new System.Drawing.Point(408, 54);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(383, 50);
+            this.label1.Size = new System.Drawing.Size(319, 41);
             this.label1.TabIndex = 10;
             this.label1.Text = "THỐNG KÊ SINH VIÊN";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // frmStudentAnalysis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1311, 732);
+            this.ClientSize = new System.Drawing.Size(1165, 586);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.studentDataGridView);
             this.Controls.Add(this.label1);
