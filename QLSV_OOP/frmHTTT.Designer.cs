fc267e51c61c50666d6a379a48413d386b8de24c
Quach Thai Duong <97613935+sukcheon22@users.noreply.github.com>
2024-01-04 11:07:03 +0700

    Add files via upload

---
+++ QLSV_OOP/frmHTTT.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace QLSV_OOP
+{
+    partial class frmHTTTAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exportFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.htttDataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.htttDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportFileToolStripMenuItem,
+            this.backToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1242, 33);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exportFileToolStripMenuItem
+            // 
+            this.exportFileToolStripMenuItem.BackColor = System.Drawing.Color.RoyalBlue;
+            this.exportFileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportFileToolStripMenuItem.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.exportFileToolStripMenuItem.Name = "exportFileToolStripMenuItem";
+            this.exportFileToolStripMenuItem.Size = new System.Drawing.Size(100, 29);
+            this.exportFileToolStripMenuItem.Text = "Xuất file";
+            this.exportFileToolStripMenuItem.Click += new System.EventHandler(this.exportFileToolStripMenuItem_Click);
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backToolStripMenuItem.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.backToolStripMenuItem.Text = "Quay lại";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(394, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 50);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "HÌNH THỨC THANH TOÁN";
+            // 
+            // htttDataGridView
+            // 
+            //this.empDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            //this.empDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            //this.empDataGridView.Location = new System.Drawing.Point(49, 109);
+            //this.empDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            //this.empDataGridView.Name = "empDataGridView";
+            //this.empDataGridView.RowHeadersWidth = 51;
+            //this.empDataGridView.Size = new System.Drawing.Size(1006, 408);
+            //this.empDataGridView.TabIndex = 15;
+            //this.empDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.empDataGridView_CellContentClick);
+            this.htttDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.htttDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.htttDataGridView.Location = new System.Drawing.Point(55, 136);
+            this.htttDataGridView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.htttDataGridView.Name = "htttDataGridView";
+            this.htttDataGridView.RowHeadersWidth = 51;
+            this.htttDataGridView.Size = new System.Drawing.Size(1132, 510);
+            this.htttDataGridView.TabIndex = 15;
+            // 
+            // frmHTTTAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 684);
+            this.Controls.Add(this.htttDataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmHTTTAnalysis";
+            this.Text = "Thống kê hình thức thanh toán";
+            this.Load += new System.EventHandler(this.frmHTTTAnalysis_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.htttDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exportFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView htttDataGridView;
+    }
+}
---
+++ QLSV_OOP/frmHTTT.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using QLSV_OOP.DAO;
+using QLSV_OOP.DTO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace QLSV_OOP
+{
+    public partial class frmHTTTAnalysis : Form
+    {
+        public frmHTTTAnalysis()
+        {
+            InitializeComponent();
+        }
+        private void frmHTTTAnalysis_Load(object sender, EventArgs e)
+        {
+            htttDataGridView.DataSource = HTTTDAO.Instance.htttGridView();
+        }
+        private void exportFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+
+            // Xuất dữ liệu từ DataGridView
+            for (int i = 0; i < htttDataGridView.Columns.Count; i++)
+            {
+                worksheet.Cells[1, i + 1] = htttDataGridView.Columns[i].HeaderText;
+            }
+
+            for (int i = 0; i < htttDataGridView.Rows.Count; i++)
+            {
+                for (int j = 0; j < htttDataGridView.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = htttDataGridView[j, i].Value;
+                }
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files|*.xlsx|All Files|*.*";
+            saveFileDialog.FileName = "HTTT_ThongKe.xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialog.FileName);
+                workbook.Close();
+                excelApp.Quit();
+
+                MessageBox.Show("Xuất file Excel thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void empDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
