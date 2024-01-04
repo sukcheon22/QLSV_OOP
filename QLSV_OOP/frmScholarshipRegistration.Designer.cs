096cc8e5f56f26ac74c6877dfcc8acce76d77798
Quach Thai Duong <97613935+sukcheon22@users.noreply.github.com>
2024-01-04 11:11:11 +0700

    Add files via upload

---
+++ QLSV_OOP/frmScholarshipRegistration.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace QLSV_OOP
+{
+    partial class frmScholarshipRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empDataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScholarshipID = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.empDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1165, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backToolStripMenuItem.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.backToolStripMenuItem.Text = "Quay lại";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // empDataGridView
+            // 
+            this.empDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.empDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.empDataGridView.Location = new System.Drawing.Point(494, 129);
+            this.empDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.empDataGridView.Name = "empDataGridView";
+            this.empDataGridView.RowHeadersWidth = 51;
+            this.empDataGridView.Size = new System.Drawing.Size(646, 434);
+            this.empDataGridView.TabIndex = 14;
+            this.empDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.empDataGridView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(412, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 41);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ĐĂNG KÝ HỌC BỔNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 24);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Mã học bổng";
+            // 
+            // txtScholarshipID
+            // 
+            this.txtScholarshipID.Location = new System.Drawing.Point(199, 148);
+            this.txtScholarshipID.Name = "txtScholarshipID";
+            this.txtScholarshipID.Size = new System.Drawing.Size(134, 22);
+            this.txtScholarshipID.TabIndex = 17;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(39, 216);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(95, 44);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(308, 216);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(95, 44);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmScholarshipRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 586);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtScholarshipID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.empDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmScholarshipRegistration";
+            this.Text = "frmScholarshipRegistration";
+            this.Load += new System.EventHandler(this.frmScholarshipRegistration_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.empDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.DataGridView empDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtScholarshipID;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ QLSV_OOP/frmScholarshipRegistration.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLSV_OOP.DTO;
+using QLSV_OOP.DAO;
+
+namespace QLSV_OOP
+{
+    public partial class frmScholarshipRegistration : Form
+    {
+        string MaSV;
+        public frmScholarshipRegistration(Sinh_Vien sinhVien)
+        {
+            InitializeComponent();
+            empDataGridView.DataSource = classGridView(sinhVien.StudentID);
+            MaSV = sinhVien.StudentID;
+        }
+
+        SqlConnection con = new SqlConnection(ConnectionString.connectionString);
+
+        private DataTable classGridView(string maSV)
+        {
+            string query = "select Tinh_trang_HB.MaHB as \"Mã học bổng\", TenHB as \"Tên học bổng\", Loai as \"Loại\" from Tinh_trang_HB "+
+                           "inner join HB on Tinh_trang_HB.MaHB = HB.MaHB " +
+                           "where MaSV = "+ maSV +";";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            return dt;
+        }
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InitializeDataGridView()
+        {
+            empDataGridView.DataSource = classGridView(MaSV);
+        }
+
+        private void DangKyMoi(string maHB, string maSV)
+        {
+            try
+            {
+                if (HBDAO.Instance.KiemTraTrung(maSV, maHB))
+                {
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO Tinh_trang_HB (MaSV, MaHB, TinhTrang) VALUES ('" + maSV + "', '" + maHB + "','False')", con))
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+
+                        con.Close();
+                    }
+                    MessageBox.Show("Đã đăng ký học bổng thành công!");
+                }
+                
+                else 
+                {
+                    MessageBox.Show("Học bổng đã được đăng ký", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                // Xử lý lỗi SQL
+                if (ex.Number == 547)  // 547 là mã lỗi cho việc vi phạm khóa ngoại
+                {
+                    MessageBox.Show("Mã học bổng không tồn tại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show($"Lỗi SQL: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi khác (nếu có)
+
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Đảm bảo rằng kết nối sẽ được đóng dù có lỗi hay không
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string maHB = txtScholarshipID.Text;
+            string maSV = MaSV;
+            DangKyMoi(maHB, maSV);
+            InitializeDataGridView();
+        }
+
+
+
+        private void empDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void XoaHocBong(string maHB, string maSV)
+        {
+            using (SqlCommand cmd = new SqlCommand("DELETE FROM Tinh_trang_HB WHERE MaSV = '" + maSV + "' AND MaHB = '" + maHB + "'", con))
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        private bool CheckXoa(string maHB, string maSV)
+        {
+            string query = "select TinhTrang from Tinh_trang_HB where MaSV = '" + maSV + "' and MaHB = '" + maHB + "';";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            return Convert.ToBoolean(dt.Rows[0][0]);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (empDataGridView.SelectedRows.Count > 0)
+                {
+                    // Lấy giá trị cần xóa từ hàng được chọn
+                    DataGridViewRow selectedRow = empDataGridView.SelectedRows[0];
+                    string maHB = selectedRow.Cells["Mã học bổng"].Value.ToString();
+                    if (CheckXoa(maHB, MaSV))
+                    {
+                        MessageBox.Show("Học bổng đã được duyệt, không thể xóa", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        XoaHocBong(maHB, MaSV);
+                        InitializeDataGridView();
+                        MessageBox.Show("Đã xóa thông tin thành công!");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng chọn một hàng để xóa!");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi khác (nếu có)
+
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Đảm bảo rằng kết nối sẽ được đóng dù có lỗi hay không
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+
+        }
+
+        private void frmScholarshipRegistration_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
