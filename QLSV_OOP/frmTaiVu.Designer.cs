ce60d81067a98ba75dd1a107a14d66ca711b67a2
Quach Thai Duong <97613935+sukcheon22@users.noreply.github.com>
2024-01-04 11:17:30 +0700

    Add files via upload

---
+++ QLSV_OOP/frmTaiVu.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace QLSV_OOP
+{
+    partial class frmTaiVu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1071, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1071, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            this.menuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1059, 541);
+            this.panel1.TabIndex = 2;
+            // 
+            // frmTaiVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 604);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmTaiVu";
+            this.Text = "frmTaiVu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTaiVu_FormClosing);
+            this.Load += new System.EventHandler(this.frmTaiVu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ QLSV_OOP/frmTaiVu.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLSV_OOP;
+using QLSV_OOP.DAO;
+using QLSV_OOP.DTO;
+
+namespace QLSV_OOP
+{
+    public partial class frmTaiVu : Form
+    {
+        Account account;
+        CapNhatHTTT capNhatHTTT = new CapNhatHTTT();
+        public frmTaiVu(Account acc)
+        {
+            InitializeComponent();
+            ToolStripMenuItem daotao = RoleDAO.CreateDaoTao();
+            ToolStripMenuItem hocbong = RoleDAO.CreateHB();
+            ToolStripMenuItem taivu = RoleDAO.CreateTaiVu();
+            ToolStripMenuItem thongke = RoleDAO.CreateThongKe();
+            menuStrip1.Items.AddRange(new ToolStripItem[] { daotao, hocbong, taivu, thongke });
+            string roleid = acc.RoleID;
+            List<string> itemsSelected = CustomizeMenuStrip.Instance.RetrieveRole(roleid);
+            CustomizeMenuStrip.Instance.Customize(menuStrip1, itemsSelected);
+            string userid = acc.UserID;
+            Nhan_vien nhanVien = Nhan_vienDAO.Instance.GetNhanVienbyUserID(userid);
+            CustomizeMenuStrip.Instance.CustomizeAccount(menuStrip2, nhanVien.StaffName);
+            //CustomizeMenuStrip.Instance.SignOut.Click += new System.EventHandler(this.SignOut);
+            CustomizeMenuStrip.Instance.SignOut.Click += SignOut;
+            CustomizeMenuStrip.Instance.ChangePassword.Click += ChangePassword;
+            capNhatHTTT.Click += CapNhatHTTT_Clicked;
+            RoleDAO.CapNhatDiemClicked += CapNhatDiem_Clicked;
+            RoleDAO.DkyLopClicked += DkyLop_Clicked;
+            RoleDAO.TCuuTKBClicked += TCuuTK_Clicked;
+            RoleDAO.TCuuKQHTClicked += TCuuKQHT_Clicked;
+            RoleDAO.CapNhatTTHBClicked += CapNhatTTHB_Clicked;
+            RoleDAO.PheDuyetYCHBClicked += PheDuyetYCHB_Clicked;
+            RoleDAO.DKHocBongClicked += DKHocBongB_Clicked;
+            RoleDAO.CapNhatHTTTClicked += CapNhatHTTT_Clicked;
+            RoleDAO.CapNhatCongNoClicked += CapNhatCongNo_Clicked;
+            RoleDAO.TTinDuNoClicked += TTinDuNo_Clicked;
+            RoleDAO.HocPhiClicked += HocPhi_Clicked;
+            RoleDAO.NoDongHocPhiClicked += NoDongHocPhi_Clicked;
+            RoleDAO.TkeDiemClicked += TkeDiem_Clicked;
+            RoleDAO.TkeHocBongClicked += TkeHocBong_Clicked;
+            RoleDAO.LopHocClicked += LopHoc_Clicked;
+            RoleDAO.HTThanhToanClicked += HTThanhToan_Clicked;
+            panel1.Controls.Add(capNhatHTTT);
+            capNhatHTTT.Location = new System.Drawing.Point(0, 0);
+            foreach (Control control in panel1.Controls)
+            {
+                control.Visible = false;
+            }
+        }
+
+        private void TkeHocBong_Clicked(object sender, EventArgs e)
+        {
+            FunctionMenuStrip.Instance.ScholarshipAnalysis(this);
+        }
+
+        private void TTinDuNo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void CapNhatDiem_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện CapNhatDiemClicked xảy ra
+        }
+
+        private void DkyLop_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện DkyLopClicked xảy ra
+        }
+
+        private void TCuuTK_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện TCuuTKBClicked xảy ra
+        }
+
+        private void TCuuKQHT_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện TCuuKQHTClicked xảy ra
+        }
+
+        private void CapNhatTTHB_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện CapNhatTTHBClicked xảy ra
+        }
+
+        private void PheDuyetYCHB_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện PheDuyetYCHBClicked xảy ra
+        }
+
+        private void DKHocBongB_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện DKHocBongBClicked xảy ra
+        }
+
+        private void CapNhatHTTT_Clicked(object sender, EventArgs e)
+        {
+            foreach (Control control in panel1.Controls)
+            {
+                control.Visible = false;
+            }
+            capNhatHTTT.Visible = true;
+        }
+
+        private void CapNhatCongNo_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện CapNhatCongNoClicked xảy ra
+        }
+
+        private void KTraDuNo_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện KTraDuNoClicked xảy ra
+        }
+
+        private void HocPhi_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện HocPhiClicked xảy ra
+            FunctionMenuStrip.Instance.TuitionAnalysis(this);
+        }
+
+        private void NoDongHocPhi_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện NoDongHocPhiClicked xảy ra
+            FunctionMenuStrip.Instance.TuitionOweAnalysis(this);
+        }
+        private void TkeDiem_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện TkeDiemClicked xảy ra
+            FunctionMenuStrip.Instance.GradeAnalysis(this);
+        }
+
+
+
+        private void LopHoc_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện LopHocClicked xảy ra
+            FunctionMenuStrip.Instance.ClassAnalysis(this);
+        }
+
+        private void HTThanhToan_Clicked(object sender, EventArgs e)
+        {
+            // Xử lý logic khi sự kiện HTThanhToanClicked xảy ra
+            FunctionMenuStrip.Instance.HTTTAnalysis(this);
+        }
+
+        
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void frmTaiVu_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SignOut(object sender, EventArgs e)
+        {
+            //this.Close();
+            FunctionMenuStrip.Instance.SignOut(this);
+        }
+
+        private void ChangePassword(object sender, EventArgs e)
+        {
+            FunctionMenuStrip.Instance.ChangePassword(this, account);
+        }
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void frmTaiVu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận đăng xuất?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
